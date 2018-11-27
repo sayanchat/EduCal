222a147058d26f6c39b876a8c6d9189712100698
sayanchat <sayanc00@gmail.com>
2018-11-26 23:43:53 -0800

    Initial Commit

---
+++ EduCal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EduCal
+//
+//  Created by Sayan Chatterjee on 11/26/18.
+//  Copyright © 2018 Sayan Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
